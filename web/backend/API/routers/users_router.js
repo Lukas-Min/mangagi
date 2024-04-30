9b8acfb27d826d1d04888e21f768b0d832007b9d
Lukas <132744951+Lukas-Min@users.noreply.github.com>
2024-04-30 14:27:29 +0800

    Delete web directory

--- web/backend/API/routers/users_router.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-
-const userController = require('../controllers/users_controllers')
-
-const userRouter = express.Router()
-
-userRouter.post('/register', userController.register)
-
-module.exports = userRouter
