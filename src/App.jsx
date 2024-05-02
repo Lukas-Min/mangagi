6207098ea99cdd6b5885cb860c07dc593b0629bc
Lukas <132744951+Lukas-Min@users.noreply.github.com>
2024-05-02 09:33:47 +0800

    Created Template for pages, components

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+
+// * Components
+import MainNav from './components/MainNav'
+function App() {
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route ></Route>
+        </Routes>
+      </Router>
+    </>
+  )
+}
+
+export default App
---
+++ src/pages/VehicleList.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const VehicleList = () => {
+  return (
+    <>
+
+    </>
+  );
+}
+
+export default VehicleList;
