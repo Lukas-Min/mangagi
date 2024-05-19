5482a8666407fe1c658272bcb373c0e1f40a2f9e
NRP7 <nathan.ronnie.paje@gmail.com>
2024-05-19 13:59:33 +0800

    Resolved Tailwind Content Problem and Added Backend Config
    Add a few directories to match the configuration for tailwind.config.js.
    
    I have also added a nodemon configuration for the backend.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,17 @@
-import React from 'react'
+// import React from 'react'
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+// PAGES
+import Home from "./pages/Home";
 
 export const App = () => {
   return (
-    <div>App</div>
+    <Router>
+        <Routes>
+            <Route path="/" element={<Home />} />
+        </Routes>
+    </Router>
   )
 }
+
+export default App;
