90ca448aa41f9bb1e62618e01d32f8995688f0c1
Lukas <132744951+Lukas-Min@users.noreply.github.com>
2024-05-20 13:02:11 +0800

    Update Navbar.jsx

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -3,12 +3,14 @@ import { Link } from 'react-router-dom'
 
 const Navbar = () => {
   return (
-    <div className='flex align-middle justify-center gap-4 p-5 bg-raisin text-rose'>
+    <div className='flex items-center justify-around p-5 bg-licorice border-b-blush border-b-2'>
       <img className='h-16 w-auto' src="./public/img/logo/mangagi.png" alt="Mangagi Logo" />
-      <Link className='' to={"/"}>Home</Link>
-      <Link to={"/about"}>About</Link>
-      <Link to={"/add-manga"}>Add Manga</Link>
-      <Link to={"/edit-manga"}>Edit Manga</Link>
+      <div className='flex gap-4'>
+        <Link to={"/"}>Home</Link>
+        <Link to={"/about"}>About</Link>
+        <Link to={"/add-manga"}>Add Manga</Link>
+        <Link to={"/edit-manga"}>Edit Manga</Link>
+      </div>
     </div>
   )
 }
