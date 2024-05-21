4a59104e3f4ad9a61fc12323aee294e8e915055a
NRP7 <nathan.ronnie.paje@gmail.com>
2024-05-21 20:04:28 +0800

    🔧Modified Frontend Source Attribute
    I have shortened the link of the source attribute for the logo called in the 'index.html' and 'Navbar' component.

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 const Navbar = () => {
   return (
     <div className='flex items-center justify-around p-5 bg-licorice border-b-blush border-b-2'>
-      <img className='h-16 w-auto' src="./public/img/logo/mangagi.png" alt="Mangagi Logo" />
+      <img className='h-16 w-auto' src="/img/logo/mangagi.png" alt="Mangagi Logo" />
       <div className='flex gap-4'>
         <Link to={"/"}>Home</Link>
         <Link to={"/about"}>About</Link>
