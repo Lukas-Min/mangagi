84083ea0ab9a8474e72b69c50f3706f76f438eff
Lukas <132744951+Lukas-Min@users.noreply.github.com>
2024-05-25 11:47:17 +0800

    Updated the Navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -18,22 +18,22 @@ class Navbar extends React.Component {
 
   render() {
     return (
-      <nav className="bg-gray-800 p-4">
+      <nav className="border-b-2 border-b-blush p-4 z-10">
         <div className="container mx-auto flex justify-between items-center">
           <div>
             <Link to={"/"}><img className='h-auto w-20 sm:w-24 md:w-28 lg:w-36' src="/img/logo/mangagi.png" alt="Mangagi Logo" /></Link>
           </div>
-          <div className="hidden md:block">
-            <ul className="flex space-x-4">
-              <Link to={"/"}>Home</Link>
-              <Link to={"/about"}>About</Link>
-              <Link to={"/add-manga"}>Add Manga</Link>
-              <Link to={"/edit-manga"}>Edit Manga</Link>
-              <Link to={"/view-manga"}>View Manga</Link>
-              <Link to={"/search-manga"}>Search Manga</Link>
+          <div className="hidden lg:block">
+            <ul className="flex space-x-4 text-md">
+              <Link className='px-5 py-1 hover:bg-blush rounded-lg' to={"/"}>Home</Link>
+              <Link className='px-5 py-1 hover:bg-blush rounded-lg' to={"/about"}>About</Link>
+              <Link className='px-5 py-1 hover:bg-blush rounded-lg' to={"/add-manga"}>Add Manga</Link>
+              <Link className='px-5 py-1 hover:bg-blush rounded-lg' to={"/edit-manga"}>Edit Manga</Link>
+              <Link className='px-5 py-1 hover:bg-blush rounded-lg' to={"/view-manga"}>View Manga</Link>
+              <Link className='px-5 py-1 hover:bg-blush rounded-lg' to={"/search-manga"}>Search Manga</Link>
             </ul>
           </div>
-          <div className="block md:hidden">
+          <div className="block lg:hidden">
             {/* Hamburger menu icon */}
             <button className=" focus:outline-none" onClick={this.toggleMenu}>
               <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
@@ -44,14 +44,14 @@ class Navbar extends React.Component {
         </div>
         {/* Mobile menu */}
         {this.state.isMenuOpen && (
-          <div className="md:hidden bg-gray-800">
-            <ul className=" my-4">
-              <Link className='block px-4 py-2' to={"/"}>Home</Link>
-              <Link className='block px-4 py-2' to={"/about"}>About</Link>
-              <Link className='block px-4 py-2' to={"/add-manga"}>Add Manga</Link>
-              <Link className='block px-4 py-2' to={"/edit-manga"}>Edit Manga</Link>
-              <Link className='block px-4 py-2' to={"/view-manga"}>View Manga</Link>
-              <Link className='block px-4 py-2' to={"/search-manga"}>Search Manga</Link>
+          <div className="lg:hidden">
+            <ul className=" my-4 text-sm">
+              <Link className='block px-4 py-2 hover:bg-blush rounded-lg' to={"/"}>Home</Link>
+              <Link className='block px-4 py-2 hover:bg-blush rounded-lg' to={"/about"}>About</Link>
+              <Link className='block px-4 py-2 hover:bg-blush rounded-lg' to={"/add-manga"}>Add Manga</Link>
+              <Link className='block px-4 py-2 hover:bg-blush rounded-lg' to={"/edit-manga"}>Edit Manga</Link>
+              <Link className='block px-4 py-2 hover:bg-blush rounded-lg' to={"/view-manga"}>View Manga</Link>
+              <Link className='block px-4 py-2 hover:bg-blush rounded-lg' to={"/search-manga"}>Search Manga</Link>
             </ul>
           </div>
         )}
