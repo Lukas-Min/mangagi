9efd78b6a21f5ffdbc4c4fe514b56222aa6c11c5
Lukas <132744951+Lukas-Min@users.noreply.github.com>
2024-05-26 18:01:02 +0800

    Changed height position and changed the color theme

--- frontend/src/pages/SearchManga.jsx
+++ frontend/src/pages/SearchManga.jsx
@@ -73,13 +73,13 @@ const SearchManga = () => {
     }, [mangaInfo]);
     return (
 
-        <div className="max-w-md mx-auto mt-[25vh]">
+        <div className="max-w-md mx-auto mt-[10vh]">
             <h1 className='text-center font-extrabold text-4xl mb-6 text-nowrap'>MangaGing Search Bar</h1>
             <form onSubmit={(e) => e.preventDefault()}>
-                <label htmlFor="default-search" className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
+                <label htmlFor="default-search" className="mb-2 text-sm font-medium text-licorice sr-only">Search</label>
                 <div className="relative">
                     <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
-                        <svg className="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                        <svg className="w-4 h-4 text-licorice" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
                             <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
                         </svg>
                     </div>
@@ -89,31 +89,31 @@ const SearchManga = () => {
                         onChange={(e) => setMangaInfo(e.target.value)}
                         type="search"
                         id="default-search"
-                        className="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                        className="block w-full p-4 ps-10 text-sm text-licorice border border-blush rounded-lg bg-gray-50 focus:ring-amaranth focus:border-amaranth"
                         placeholder="Manga Title"
                         required
                     />
                     {hasSearched && mangaResults.length > 0 && (
-                        <div className="absolute z-10 w-full mt-2 bg-white border border-gray-300 rounded-lg shadow-lg dark:bg-gray-800 dark:border-gray-600 max-h-60 overflow-y-auto">
+                        <div className="absolute z-10 w-full mt-2 bg-raisin border border-blush rounded-lg shadow-lg max-h-60 overflow-y-auto">
                             {mangaResults.map(manga => (
                                 <a
                                     key={manga.id}
                                     href={`https://mangadex.org/title/${manga.id}`}
                                     target="_blank"
                                     rel="noopener noreferrer"
-                                    className="block p-4 border-b border-gray-200 hover:bg-gray-100 dark:border-gray-700 dark:hover:bg-gray-700"
+                                    className="block p-4 border-b border-amaranth hover:bg-amaranth"
                                 >
                                     <div className="flex items-center">
                                         <img src={`https://uploads.mangadex.org/covers/${manga.id}/${manga.fileName}`} alt={manga.name} className="w-30 h-40 rounded-lg mr-4" />
-                                        <span className="text-gray-900 dark:text-white font-bold">{manga.name}</span>
+                                        <span className="text-rose font-bold">{manga.name}</span>
                                     </div>
                                 </a>
                             ))}
                         </div>
                     )}
                     {hasSearched && mangaResults.length === 0 && (
-                        <div className="absolute z-10 w-full mt-2 bg-white border border-gray-300 rounded-lg shadow-lg dark:bg-gray-800 dark:border-gray-600 max-h-60 overflow-y-auto">
-                            <div className="p-4 text-gray-900 dark:text-white font-bold">No manga found</div>
+                        <div className="absolute z-10 w-full mt-2 bg-white border border-blush rounded-lg shadow-lg max-h-60 overflow-y-auto">
+                            <div className="p-4 text-rose font-bold">No manga found</div>
                         </div>
                     )}
                 </div>
