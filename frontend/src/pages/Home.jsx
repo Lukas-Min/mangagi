82c10bb452a24098b86cb36d7da70c46b568d7ac
Lukas <132744951+Lukas-Min@users.noreply.github.com>
2024-05-26 17:00:20 +0800

    Changed the structure of Home Page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,19 +1,12 @@
 import React from 'react';
-import MangaCover from "../components/MangaCover";
+import MangaCover from '../components/MangaCover';
 
 const Home = () => {
-    return (
-    <div className="container mx-auto px-5 py-8">
-      <div className="grid gap-y-6 gap-x-3 justify-center px-0 md:px-28" style={{ gridTemplateColumns: 'repeat(auto-fill, minmax(250px, 1fr))' }}>
-        {Array.from({ length: 15 }).map((_, index) => (
-          <div key={index} className="flex justify-center">
-            <MangaCover index={index} />
-          </div>
-        ))}
-      </div>
+  return (
+    <div className="container mx-auto px-[5vw]">
+      <MangaCover />
     </div>
-    );
-    
+  );
 }
 
 export default Home;
