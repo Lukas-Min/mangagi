8cb62a039310189b373048355cd504beac9008bb
Lukas <132744951+Lukas-Min@users.noreply.github.com>
2024-05-27 18:23:44 +0800

    Added a transition effect for smoother interaction

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -18,19 +18,19 @@ class Navbar extends React.Component {
 
   render() {
     return (
-      <nav className="border-b-2 border-b-blush p-4 z-10 top-0 sticky bg-licorice mb-10">
+      <nav className="border-b-2 border-b-blush p-4 z-20 top-0 sticky bg-licorice mb-10">
         <div className="container mx-auto flex justify-between items-center">
           <div>
             <Link to={"/"}><img className='h-auto w-24 md:w-28 lg:w-36' src="/img/logo/mangagi.png" alt="Mangagi Logo" /></Link>
           </div>
           <div className="hidden lg:block">
             <ul className="flex space-x-4">
-              <Link className='px-5 py-1 hover:bg-blush rounded-lg' to={"/"}>Home</Link>
-              <Link className='px-5 py-1 hover:bg-blush rounded-lg' to={"/about"}>About</Link>
-              <Link className='px-5 py-1 hover:bg-blush rounded-lg' to={"/add-manga"}>Add Manga</Link>
-              <Link className='px-5 py-1 hover:bg-blush rounded-lg' to={"/edit-manga"}>Edit Manga</Link>
-              <Link className='px-5 py-1 hover:bg-blush rounded-lg' to={"/view-manga"}>View Manga</Link>
-              <Link className='px-5 py-1 hover:bg-blush rounded-lg' to={"/search-manga"}>Search Manga</Link>
+              <Link className='px-5 py-1 hover:bg-blush transition-colors rounded-lg' to={"/"}>Home</Link>
+              <Link className='px-5 py-1 hover:bg-blush transition-colors rounded-lg' to={"/about"}>About</Link>
+              <Link className='px-5 py-1 hover:bg-blush transition-colors rounded-lg' to={"/add-manga"}>Add Manga</Link>
+              <Link className='px-5 py-1 hover:bg-blush transition-colors rounded-lg' to={"/edit-manga"}>Edit Manga</Link>
+              <Link className='px-5 py-1 hover:bg-blush transition-colors rounded-lg' to={"/view-manga"}>View Manga</Link>
+              <Link className='px-5 py-1 hover:bg-blush transition-colors rounded-lg' to={"/search-manga"}>Search Manga</Link>
             </ul>
           </div>
           <div className="block lg:hidden">
@@ -46,12 +46,12 @@ class Navbar extends React.Component {
         {this.state.isMenuOpen && (
           <div className="lg:hidden">
             <ul className=" my-4">
-              <Link className='block px-4 py-2 hover:bg-blush rounded-lg' to={"/"}>Home</Link>
-              <Link className='block px-4 py-2 hover:bg-blush rounded-lg' to={"/about"}>About</Link>
-              <Link className='block px-4 py-2 hover:bg-blush rounded-lg' to={"/add-manga"}>Add Manga</Link>
-              <Link className='block px-4 py-2 hover:bg-blush rounded-lg' to={"/edit-manga"}>Edit Manga</Link>
-              <Link className='block px-4 py-2 hover:bg-blush rounded-lg' to={"/view-manga"}>View Manga</Link>
-              <Link className='block px-4 py-2 hover:bg-blush rounded-lg' to={"/search-manga"}>Search Manga</Link>
+              <Link className='block px-4 py-2 hover:bg-blush transition-colors rounded-lg' to={"/"}>Home</Link>
+              <Link className='block px-4 py-2 hover:bg-blush transition-colors rounded-lg' to={"/about"}>About</Link>
+              <Link className='block px-4 py-2 hover:bg-blush transition-colors rounded-lg' to={"/add-manga"}>Add Manga</Link>
+              <Link className='block px-4 py-2 hover:bg-blush transition-colors rounded-lg' to={"/edit-manga"}>Edit Manga</Link>
+              <Link className='block px-4 py-2 hover:bg-blush transition-colors rounded-lg' to={"/view-manga"}>View Manga</Link>
+              <Link className='block px-4 py-2 hover:bg-blush transition-colors rounded-lg' to={"/search-manga"}>Search Manga</Link>
             </ul>
           </div>
         )}
