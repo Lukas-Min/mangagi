cd60af049efa82ddd467431786f90cf07e0b38de
NRP7 <nathan.ronnie.paje@gmail.com>
2024-05-27 23:41:16 +0800

    📱Created Delete Button Component
    I have converted the delete button into a component.

---
+++ frontend/src/components/DeleteButton.jsx
@@ -0,0 +1,11 @@
+
+
+const DeleteButton = () => {
+    return (
+        <button className="h-12 rounded-3xl bg-amaranth w-60">
+            <h1 className="text-sm text-center font-bold">Delete</h1>
+        </button>
+    )
+}
+
+export default DeleteButton;
