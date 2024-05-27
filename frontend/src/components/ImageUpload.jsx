b217b192da5afa9bb331e9d5f549fbd671a6bd67
Lukas <132744951+Lukas-Min@users.noreply.github.com>
2024-05-27 19:08:50 +0800

    Added transition

--- frontend/src/components/ImageUpload.jsx
+++ frontend/src/components/ImageUpload.jsx
@@ -21,7 +21,7 @@ const ImageUpload = () => {
   return (
     <>
       <label htmlFor="MangaImage" className="relative cursor-pointer group w-full h-full bg-raisin">
-        <div className="rounded-lg flex items-center justify-center w-full h-[40vh] lg:h-full">
+        <div className="rounded-lg flex items-center justify-center w-full h-[40vh] lg:h-full hover:bg-black hover:bg-opacity-30 transition-colors duration-300">
           {imageSrc ? (
             <div className="w-full h-full relative">
               <img id="the-picture" src={imageSrc} alt="Uploaded" className="w-full h-full object-contain lg:object-cover rounded"
@@ -29,12 +29,12 @@ const ImageUpload = () => {
               <button type="button" onMouseDown={removeImage} className="absolute top-2 right-2 m-2 bg-amaranth text-rose font-bold px-3 py-1 rounded-lg hover:bg-blush transition-colors z-10">
                 Remove
               </button>
-              <div className="absolute text-lg inset-0 flex items-center justify-center bg-black bg-opacity-50 text-white opacity-0 group-hover:opacity-100">
+              <div className="absolute text-lg inset-0 flex items-center justify-center transition-opacity duration-300 bg-black bg-opacity-70 text-white opacity-0 group-hover:opacity-100 font-bold">
                 Click to upload
               </div>
             </div>
           ) : (
-            <div className="text-lg flex items-center justify-center text-rose">
+            <div className="text-lg flex items-center justify-center font-bold text-rose ">
               Click to upload
             </div>
           )}
