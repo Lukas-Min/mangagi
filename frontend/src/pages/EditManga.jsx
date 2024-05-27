2f349b5f2ab5542169852fca83a6f4fc96e437a8
Lukas <132744951+Lukas-Min@users.noreply.github.com>
2024-05-27 19:09:51 +0800

    Update EditManga.jsx

--- frontend/src/pages/EditManga.jsx
+++ frontend/src/pages/EditManga.jsx
@@ -8,14 +8,9 @@ const EditManga = () => {
       <div className="lg:col-span-2 border-2 border-rose rounded-lg bg-raisin px-8 py-6">
         <h1 className="text-4xl font-bold my-6 text-center">Edit Manga</h1>
         <MangaForm />
-        <div className="flex justify-center">
-          <button className="px-6 py-2 w-full rounded-lg bg-amaranth hover:bg-blush transition-colors text-white my-3">
-            <h1 className="text-sm text-center font-bold">Edit</h1>
-          </button>
-        </div>
       </div>
-      <div className="col-span-1 border-2 border-rose rounded-lg bg-raisin flex items-center justify-center lg:max-h-none">
-        <div className="w-full h-full lg:max-h-full">
+      <div className="lg:col-span-1 border-2 border-rose rounded-lg bg-raisin flex items-center justify-center">
+        <div className="w-full h-full">
           <ImageUpload />
         </div>
       </div>
