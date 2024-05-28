da47d6c454190355932485c210200033c5e7aa45
Lukas <132744951+Lukas-Min@users.noreply.github.com>
2024-05-28 21:00:35 +0800

    Gave proper spacing, sizing and transition effect for the buttons

--- frontend/src/components/EditButton.jsx
+++ frontend/src/components/EditButton.jsx
@@ -2,8 +2,8 @@
 
 const EditButton = () => {
     return (
-        <button className="h-12  rounded-3xl bg-amaranth w-60 me-3">
-            <h1 className="text-sm text-center font-bold">Edit</h1>
+        <button className="h-12 rounded-xl bg-amaranth w-[30%] me-3 hover:bg-blush transition-colors duration-200">
+            <h1 className="text-center font-bold">Edit</h1>
         </button>
     )
 }
