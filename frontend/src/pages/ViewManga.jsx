4dbb8f3fd2ea069543e510b167feb96e0e70f700
Lukas <132744951+Lukas-Min@users.noreply.github.com>
2024-05-30 17:08:52 +0800

    Changed the design of the modal for delete confimation

--- frontend/src/pages/ViewManga.jsx
+++ frontend/src/pages/ViewManga.jsx
@@ -1,12 +1,7 @@
 import React, { useState } from "react";
 import { toUpperCase, isObjectId } from '../../utils';
-
-// HOOKS
 import { useNavigate, useParams } from 'react-router-dom';
 import { useViewMangaData } from '../hooks/useMangaData';
-
-
-// COMPONENTS
 import DeleteButton from '../components/DeleteButton';
 import EditButton from '../components/EditButton';
 import SaveButton from '../components/SaveButton';
@@ -21,16 +16,11 @@ import DialogTitle from '@mui/material/DialogTitle';
 const ViewManga = () => {
     const { id } = useParams();
     const navigate = useNavigate();
-
     const { data: mangaInfo, isLoading, error } = useViewMangaData(id);
-
     const [saveMessage, setSaveMessage] = useState(null);
     const [open, setOpen] = useState(false);
 
-
     const saveMangaDetails = async () => {
-
-
         try {
             const saveResponse = await fetch(`${import.meta.env.VITE_BACKEND_URI}/mangas/add`, {
                 method: 'POST',
@@ -40,24 +30,12 @@ const ViewManga = () => {
                 body: JSON.stringify(mangaInfo.data),
             });
 
-            // if (saveResponse.status === 500)
-            // {
-            //   console.log(saveResponse.message);
-            //   console.log(saveResponse.data);
-            // }
-
-            console.log(saveResponse);
-
             if (!saveResponse.ok) {
                 throw new Error('Failed to save manga details');
-            }
+            }   
 
-            // const saveData = await saveResponse.json(); //
-            // console.log('Manga saved successfully:', saveData);
             setSaveMessage({ content: 'Manga saved successfully!', severity: 'success' });
-
         } catch (err) {
-            // console.error('Error saving manga:', err);
             setSaveMessage({ content: `Error saving manga: ${err.message}`, severity: 'error' });
         }
     };
@@ -76,12 +54,9 @@ const ViewManga = () => {
                 throw new Error(`Failed to delete manga: ${errorData.message}`);
             }
 
-            // const deleteData = await deleteResponse.json();
-            // console.log('Manga deleted successfully:', deleteData);
             alert('Manga deleted successfully!');
             navigate('/');
         } catch (err) {
-            // console.error('Error deleting manga:', err);
             alert(`Error deleting manga: ${err.message}`);
         }
     };
@@ -96,7 +71,6 @@ const ViewManga = () => {
 
     return (
         <>
-
             {isLoading && (
                 <div className="flex justify-center items-center h-screen">
                     <h1>Loading...</h1>
@@ -124,7 +98,6 @@ const ViewManga = () => {
                         )}
                         <div className="w-full">
                             <h1 className="text-2xl md:text-3xl lg:text-4xl font-bold text-center lg:text-left py-2">{mangaInfo.data.title}</h1>
-
                             <div className="grid grid-cols-1 xs:grid-cols-3 gap-x-3 text-center text-nowrap">
                                 <div className="text-baseline sm:text-lg py-2 lg:py-3 lg:xl text-start col-span-1 xs:col-span-3">
                                     <h1 className="p-2">
@@ -135,22 +108,17 @@ const ViewManga = () => {
                                                 : mangaInfo.data.author.slice(0, -1).join(', ') + ', and ' + mangaInfo.data.author[mangaInfo.data.author.length - 1]}
                                     </h1>
                                 </div>
-
                                 <div className="bg-raisin rounded-lg">
                                     <h1 className="p-2">Published: {mangaInfo.data.year_published != null ? mangaInfo.data.year_published : "Unknown"}</h1>
                                 </div>
-
                                 <div className="bg-raisin rounded-lg">
                                     <h1 className="p-2">Status: {toUpperCase(mangaInfo.data.manga_status)}</h1>
                                 </div>
-
                                 <div className="bg-raisin rounded-lg">
                                     <h1 className="p-2">State: {toUpperCase(mangaInfo.data.manga_state)}</h1>
                                 </div>
                             </div>
-
                             <h1 className="text-2xl font-bold py-1 md:py-2 text-left">Tags</h1>
-
                             <div className="flex flex-wrap lg:justify-start gap-2 py-2">
                                 {mangaInfo.data.genre.map((tag, index) => (
                                     <div key={index} className="py-2 px-3 rounded-xl bg-blush text-licorice">
@@ -158,7 +126,6 @@ const ViewManga = () => {
                                     </div>
                                 ))}
                             </div>
-
                             <div className="text-justify py-5">
                                 {mangaInfo.data.description ? (
                                     mangaInfo.data.description.split('\n').map((paragraph, index) => (
@@ -171,8 +138,6 @@ const ViewManga = () => {
                                     "No available description."
                                 )}
                             </div>
-
-
                             <div className="flex justify-between my-2 items-center">
                                 {isObjectId(id) ? (
                                     <>
@@ -185,7 +150,6 @@ const ViewManga = () => {
                             </div>
                         </div>
                     </div>
-
                     <div className="order-1 lg:order-2 flex justify-center lg:mt-5">
                         <div className="max-h-full">
                             <img
@@ -195,29 +159,54 @@ const ViewManga = () => {
                             />
                         </div>
                     </div>
-
                 </section>
             )}
 
-            <Dialog open={open} onClose={handleClose}>
-                <DialogTitle>Confirm Delete</DialogTitle>
-                <DialogContent>
-                    <DialogContentText>
+            <Dialog
+                open={open}
+                onClose={handleClose}
+                sx={{
+                    '& .MuiDialog-paper': {
+                        backgroundColor: '#FFD9DA',
+                        padding: '15px',
+                    },
+                }}>
+                <DialogTitle sx={{ color: '#191516', fontSize: '1.5rem' }}>Confirm Delete</DialogTitle>
+                <DialogContent sx={{ padding: '0px', margin: '1rem 2rem' }} >
+                    <DialogContentText sx={{ color: '#191516' }}>
                         Are you sure you want to delete this manga? This action cannot be undone.
                     </DialogContentText>
                 </DialogContent>
                 <DialogActions>
-                    <Button onClick={handleClose} color="primary">
+                    <Button onClick={handleClose} sx={{
+                        backgroundColor: '#AC274F',
+                        color: '#FFD9DA',
+                        fontWeight: 'bold',
+                        '&:hover': {
+                            backgroundColor: '#EB638B',
+                            color: '#FFFFFF',
+                        },
+                    }}>
                         Cancel
                     </Button>
-                    <Button onClick={deleteManga} color="secondary" autoFocus>
+                    <Button
+                        onClick={deleteManga}
+                        sx={{
+                            backgroundColor: '#AC274F',
+                            color: '#FFD9DA',
+                            fontWeight: 'bold',
+                            '&:hover': {
+                                backgroundColor: '#EB638B',
+                                color: '#FFFFFF',
+                            },
+                        }}
+                        autoFocus>
                         Delete
                     </Button>
                 </DialogActions>
             </Dialog>
         </>
     );
-
 };
 
 export default ViewManga;
