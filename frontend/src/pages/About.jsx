5ba2b7f78dbf6f419305c3f935e2d98a364936c9
Lukas <132744951+Lukas-Min@users.noreply.github.com>
2024-05-31 01:46:43 +0800

    Update About.jsx

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -6,7 +6,7 @@ const About = () => {
       <h1 className="text-4xl font-bold mb-2 text-start">About Us</h1>
       <p className="text-4xl my-6 font-semibold">Meet the MangaGu Team!</p>
       <div className="flex flex-col lg:flex-row justify-center items-center gap-10">
-        <div className='border-2 rounded-lg border-blush shadow transform hover:scale-105 transition duration-300 cursor-pointer'>
+        <div className='border-2 rounded-lg border-blush shadow transform hover:scale-105 transition duration-300'>
           <div className="max-w-lg rounded overflow-hidden shadow-lg m-4 bg-raisin">
             <img
               src="/img/About/MorPaDe.jpg"
@@ -17,11 +17,15 @@ const About = () => {
               <div className="font-bold text-xl mb-2 text-blush">MorPaDum</div>
               <p className="text-base">
                 Moraleta as (Mor), Paje as (Pa), and Dumawal as (Dum).
+                <hr className='my-3' />
+                <span className='font-bold'>Julius Caesar Moraleta</span> as a Frontend Developer <br />
+                <span className='font-bold'>Nathan Ronnie Paje</span> as a Backend Developer <br />
+                <span className='font-bold'>Leonard John Dumawal</span> as a Backend Developer
               </p>
             </div>
           </div>
         </div>
-        <div className='border-2 rounded-lg border-blush shadow transform hover:scale-105 transition duration-300 cursor-pointer'>
+        <div className='border-2 rounded-lg border-blush shadow transform hover:scale-105 transition duration-300'>
           <div className="max-w-lg rounded overflow-hidden shadow-lg m-4 bg-raisin">
             <img
               src="/img/About/ArMal.jpg"
@@ -33,12 +37,17 @@ const About = () => {
               <p className="text-base">
                 Malaga as (Mal) and Arizala as (Ar).
               </p>
+              <p className="text-base">
+                <hr className='my-3' />
+                <span className='font-bold'>Andrea Kate Malaga</span> as a Frontend Developer <br />
+                <span className='font-bold'>Jaeni Alison Arizala</span> as a Frontend Developer <br /> <br />
+              </p>
             </div>
           </div>
         </div>
       </div>
       <p className="text-lg mt-8">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur corrupti velit, harum reiciendis temporibus debitis autem provident consequuntur aspernatur rerum dolores. Temporibus quibusdam perferendis neque unde rerum hic animi voluptatem?
+
       </p>
     </div>
   );
