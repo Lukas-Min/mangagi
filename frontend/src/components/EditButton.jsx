19e4235f4e0490b98a0d1936494f4d244de0d46c
NRP7 <nathan.ronnie.paje@gmail.com>
2024-05-31 01:15:20 +0800

    ⚡️Modified Edit Button
    The edit button now navigates to the edit page directly based on the a specific manga info id stored in the database.

--- frontend/src/components/EditButton.jsx
+++ frontend/src/components/EditButton.jsx
@@ -1,9 +1,9 @@
 import { Link } from "react-router-dom"
 
 
-const EditButton = ({ onClick }) => {
+const EditButton = ({ id, onClick }) => {
     return (
-        <Link to={"/edit-manga"} className="w-full">
+        <Link to={`/edit-manga/${id}`} className="w-full">
             <button className="h-12 rounded-xl bg-amaranth w-[30%] me-3 hover:bg-blush transition-colors duration-200"
                 onClick={onClick}>
                 <h1 className="text-center font-bold">Edit</h1>
