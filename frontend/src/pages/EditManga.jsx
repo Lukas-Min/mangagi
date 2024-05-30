2443233baa65dd60da61d248cd6c9401258f3407
NRP7 <nathan.ronnie.paje@gmail.com>
2024-05-31 01:18:21 +0800

    🎨Updated Edit Manga Details Page
    I have updated the information passed through the edit manga details page by directly fetching the manga details id from the link parameters and including it to the cancel button for a specific routing.

--- frontend/src/pages/EditManga.jsx
+++ frontend/src/pages/EditManga.jsx
@@ -1,4 +1,11 @@
 import React from 'react';
+
+
+// HOOKS
+import { useParams } from 'react-router-dom';
+
+
+// COMPONENTS
 import ImageUpload from '../components/ImageUpload';
 import MangaForm from '../components/MangaForm';
 import SaveButton from '../components/SaveButton';
@@ -6,23 +13,30 @@ import CancelButton from '../components/CancelButton';
 
 
 const EditManga = () => {
-  return (
-    <section className="grid grid-cols-1 lg:grid-cols-3 gap-8 px-[15vw] my-14">
-      <div className="lg:col-span-2 border-2 order-2 lg:order-1 border-rose rounded-lg bg-raisin px-8 py-6">
-        <h1 className="text-4xl font-bold my-6 text-center">Edit Manga</h1>
-        <MangaForm />
-        <div className="flex justify-center w-full lg:col-span-2 gap-2">
-          <SaveButton />
-          <CancelButton />
-        </div>
-      </div>
-      <div className="lg:col-span-1 border-2 order-1 lg:order-2 border-rose rounded-lg bg-raisin flex items-center justify-center">
-        <div className="w-full h-full">
-          <ImageUpload />
-        </div>
-      </div>
-    </section>
-  );
+    const { id } = useParams();
+
+    return (
+        <section className="grid grid-cols-1 lg:grid-cols-3 gap-8 px-[15vw] my-14">
+
+            <div className="lg:col-span-2 border-2 order-2 lg:order-1 border-rose rounded-lg bg-raisin px-8 py-6">
+                <h1 className="text-4xl font-bold my-6 text-center">Edit Manga</h1>
+
+                <MangaForm />
+
+                <div className="flex justify-center w-full lg:col-span-2 gap-2">
+                    <SaveButton />
+                    <CancelButton id={id} />
+                </div>
+            </div>
+
+            <div className="lg:col-span-1 border-2 order-1 lg:order-2 border-rose rounded-lg bg-raisin flex items-center justify-center">
+                <div className="w-full h-full">
+                    <ImageUpload />
+                </div>
+            </div>
+
+        </section>
+    );
 };
 
 export default EditManga;
