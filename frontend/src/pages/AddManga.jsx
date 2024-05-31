8216205a7312e37e7e4b6fae7cc92f26d4752306
Lukas <132744951+Lukas-Min@users.noreply.github.com>
2024-05-31 14:00:12 +0800

    Fixed the function name and export

--- frontend/src/pages/AddManga.jsx
+++ frontend/src/pages/AddManga.jsx
@@ -5,7 +5,7 @@ import SaveButton from '../components/SaveButton';
 import CancelButton from '../components/CancelButton';
 
 
-const EditManga = () => {
+const AddManga = () => {
   return (
     <section className="grid grid-cols-1 lg:grid-cols-3 gap-8 px-[15vw] my-14">
       <div className="lg:col-span-2 border-2 order-2 lg:order-1 border-rose rounded-lg bg-raisin px-8 py-6">
@@ -25,4 +25,4 @@ const EditManga = () => {
   );
 };
 
-export default EditManga;
+export default AddManga;
