d440b7ad8c8ad89b0e1a38e0204727e9f064ebbd
NRP7 <nathan.ronnie.paje@gmail.com>
2024-05-31 15:03:22 +0800

    ⚡️Integration of Add Manga Page
    The add manga page is now integrated with the backend function for adding manga details and storing it in the database. THIS MIGHT STILL BE A WORK IN PROGRESS.

--- frontend/src/pages/AddManga.jsx
+++ frontend/src/pages/AddManga.jsx
@@ -1,28 +1,87 @@
-import React from 'react';
+import { useState } from 'react';
+import { useLocation } from 'react-router-dom';
+import { useMutation } from '@tanstack/react-query';
+
 import ImageUpload from '../components/ImageUpload';
 import MangaForm from '../components/MangaForm';
-import SaveButton from '../components/SaveButton';
-import CancelButton from '../components/CancelButton';
+import Alert from '@mui/material/Alert';
+
+const AddManga = () => {
+  const location = useLocation();
+  const searchParams = new URLSearchParams(location.search);
+  const [imageFilename, setImageFilename] = useState('');
+  const [imageSrc, setImageSrc] = useState('');
+  const [saveMessage, setSaveMessage] = useState(null);
+
+    const addMangaMutation = useMutation({
+        mutationFn: async formData => {
+            const response = await fetch(`${import.meta.env.VITE_BACKEND_URI}/mangas/add`, {
+                method: 'POST',
+                body: JSON.stringify(formData),
+                headers: {
+                'Content-Type': 'application/json',
+                },
+            });
+    
+            // console.log(response)
+            if (!response.ok) 
+            {
+                throw new Error('Failed to add manga');
+            }
+
+            return response.json();
+
+    }});
+
+    const handleImageUpload = ({ imageName, imageSrc }) => {
+        setImageSrc(imageSrc);
+        setImageFilename(imageName);
+    };
+
+    const handleMangaSubmit = async () => {
+        const formData = {
+            title: searchParams.get('title') || '',
+            manga_id: searchParams.get('mangaId') || '',
+            year_published: searchParams.get('yearPublished') || '',
+            author: searchParams.getAll('author'),
+            manga_state: searchParams.get('state') || '',
+            manga_status: searchParams.get('status') || '',
+            chapters: searchParams.get('chapters') || '',
+            genre: searchParams.getAll('tags'),
+            description: searchParams.get('description') || '',
+            cover_art: imageFilename,
+            cover_art_src: imageSrc,
+        };
+    
+        try {
+            await addMangaMutation.mutateAsync(formData);
+            setSaveMessage({ content: 'Manga added successfully!', severity: 'success' });
+        } catch (error) {
+            setSaveMessage({ content: `${error.message}`, severity: 'error' });
+        }
+    };
 
 
-const EditManga = () => {
   return (
     <section className="grid grid-cols-1 lg:grid-cols-3 gap-8 px-[15vw] my-14">
+        
       <div className="lg:col-span-2 border-2 order-2 lg:order-1 border-rose rounded-lg bg-raisin px-8 py-6">
+        {saveMessage && (
+            <div className="w-full">
+                <Alert severity={saveMessage.severity} onClose={() => setSaveMessage(null)}>{saveMessage.content}</Alert>
+            </div>
+        )}
         <h1 className="text-4xl font-bold my-6 text-center">Add Manga</h1>
-        <MangaForm />
-        <div className="flex justify-center w-full lg:col-span-2 gap-2">
-          <SaveButton />
-          <CancelButton />
-        </div>
+        <MangaForm onSubmit={handleMangaSubmit} />
+        
       </div>
       <div className="lg:col-span-1 border-2 order-1 lg:order-2 border-rose rounded-lg bg-raisin  h-auto width-auto min-h-[30vh] max-h-[30vh] lg:max-h-[60vh]">
         <div className="w-full h-full">
-          <ImageUpload className="w-full h-full object-cover" />
+          <ImageUpload className="w-full h-full object-cover" onImageUpload={handleImageUpload} />
         </div>
       </div>
     </section>
   );
 };
 
-export default EditManga;
+export default AddManga;
